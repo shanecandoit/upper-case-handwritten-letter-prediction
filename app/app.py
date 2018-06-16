ba9b1539645bc6c81656f70248e774b03789dfc9
Shane Page <shanepage@ku.edu>
2018-06-16 13:38:27 -0500

    try app.py inside app/

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+#from flask.ext.heroku import Heroku
+#ExtDeprecationWarning: Importing flask.ext.heroku is deprecated, use flask_heroku instead.
+from flask_heroku import Heroku
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now()#.strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
